35ec9dd39b05b68e05d0a0e0309fa72adf2eff66
TPurusho <110383760+TPurusho@users.noreply.github.com>
2023-05-08 15:05:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 
 streamlit.title('Chennai super kings')
 streamlit.header('Caption - M S Dhoni')
+streamlit.text('Players list -  Ravindra Jadeja, Devon Conway, Ruturaj Gaikwad, Ambati Rayudu, Subhranshu Senapati, Moeen Ali, Shivam Dube, Rajvardhan Hangargekar, Dwaine Pretorius, Mitchell Santner, Deepak Chahar, Tushar Deshpande, Mukesh Choudhary, Matheesha Pathirana, Simarjeet Singh, Prashant Solanki, Maheesh Theekshana, Ajinkya Rahane, Ben Stokes, Shaik Rasheed, Nishant Sindhu, Kyle Jamieson, Ajay Mandal, Bhagath Varma')
