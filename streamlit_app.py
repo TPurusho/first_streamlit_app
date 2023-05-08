6e6802e96a80d024edbc4e920c1d53e3d4e4f316
TPurusho <110383760+TPurusho@users.noreply.github.com>
2023-05-08 15:16:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,8 @@
 import streamlit
 
-streamlit.title('Chennai super kings')
-streamlit.header('Caption - M S Dhoni')
-streamlit.text('Players list -  Ravindra Jadeja, Devon Conway, Ruturaj Gaikwad')
-streamlit.text('                Ambati Rayudu, Subhranshu Senapati, Moeen Ali')
-streamlit.text('                Shivam Dube, Rajvardhan Hangargekar, Dwaine Pretorius')
-streamlit.text('                Mitchell Santner, Deepak Chahar, Tushar Deshpande')
-streamlit.text('                Mukesh Choudhary, Matheesha Pathirana, Simarjeet Singh')
-streamlit.text('                Prashant Solanki, Maheesh Theekshana, Ajinkya Rahane')
-streamlit.text('                Ben Stokes, Shaik Rasheed, Nishant Sindhu')
-streamlit.text('                Kyle Jamieson, Ajay Mandal, Bhagath Varma')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hot-boiled Free-Range Egg')
 
